b5d2bfd151ca71aeb17bda50f63e5bf983e19b06
vic25rayon03 <42364302+vic25rayon03@users.noreply.github.com>
2018-08-27 13:02:13 -0500

    Update QuestionAnswerer.swift

--- Ejercicios_de_clase/QuestionAnswerer.swift
+++ Ejercicios_de_clase/QuestionAnswerer.swift
@@ -3,6 +3,16 @@ struct MyQuestionAnswerer {
         // PENDIENTE: Escribir una respuesta
         if question.hasPrefix("Whats your name?")
         {}
-        return "?"
+        if question.hasPrefix("Were are you from?")
+        {
+            print("Mexico")
+        }
+        if question.hasPrefix("How are you?")
+        {}
+        if question.hasPrefix("How old are you?")
+        {}
+        if question.hasPrefix("What your favourite color?")
+        {}
     }
+    return "?"
 }
