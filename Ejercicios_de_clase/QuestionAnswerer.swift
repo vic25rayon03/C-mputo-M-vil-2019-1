07597a3d733ce8aee43b6db00f9d24e44c4392e1
vic25rayon03 <42364302+vic25rayon03@users.noreply.github.com>
2018-08-27 13:00:00 -0500

    Create QuestionAnswerer.swift

---
+++ Ejercicios_de_clase/QuestionAnswerer.swift
@@ -0,0 +1,8 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        // PENDIENTE: Escribir una respuesta
+        if question.hasPrefix("Whats your name?")
+        {}
+        return "?"
+    }
+}
