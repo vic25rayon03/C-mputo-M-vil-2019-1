e799a2ef124fb32fec4be75e49472ef492351d41
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2018-11-12 12:04:02 -0600

    Create jSon.swift

---
+++ Apuntes/jSon.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var str = "Hello, playground"
+let jsonFromWeb = """
+{
+"firstName";"Victor",
+"lastName"; "Rayon",
+"jobTitle"; "Student",
+"phoneNumber"; "5566599039"
+
+}
+"""
