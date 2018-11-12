cb3b48f16f66fb1fc93e8f3881889126ea04eed9
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2018-11-12 12:43:04 -0600

    Update jSon.swift

--- Apuntes/jSon.swift
+++ Apuntes/jSon.swift
@@ -10,3 +10,13 @@ let jsonFromWeb = """
 
 }
 """
+
+//Transformar cadena a datos utilizando UTF8
+// """.data(using: .utf8)!
+//Estructura de datos
+struct Employer: Decodable {
+    var firstName: String
+    var lastname: String
+    var jobTitle: String
+    var phoneNumber: String
+}
